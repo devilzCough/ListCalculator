b3ae13d1fa6ed0152df1a38a933e378e9bdf51ab
devilzcough <devilzcough@gmail.com>
2022-09-13 10:59:38 +0900

    start project

---
+++ ListCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListCalculator
+//
+//  Created by jiniz.ll on 2022/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListCalculator/ListCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListCalculatorApp.swift
+//  ListCalculator
+//
+//  Created by jiniz.ll on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct ListCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
