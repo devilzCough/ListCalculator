4e7fed4a60340656c078e1502207ed4cecefac56
devilzcough <devilzcough@gmail.com>
2022-09-15 21:55:18 +0900

    feat :⚡️ ListCalculatorInputView 수정
    - 이전에 각각의 뷰를 VStack을 사용하여 배치했었는데 List로 변경하여 보다
      깔끔하게 수정함

--- ListCalculator/ListCalculatorInputView.swift
+++ ListCalculator/ListCalculatorInputView.swift
@@ -12,8 +12,7 @@ struct ListCalculatorInputView: View {
     @Binding var item: Item
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Spacer()
+        List {
             TextField("이름", text: $item.name)
             TextField("금액", value: $item.price, formatter: NumberFormatter())
             Stepper(value: $item.count, in: 1...100) {
@@ -38,15 +37,16 @@ struct ListCalculatorInputView: View {
                 }
                 .frame(maxWidth: .infinity)
             }
-            .frame(height: 50)
+            .listRowInsets(EdgeInsets())
             .background(Color.yellow)
         }
+        .listStyle(.inset)
     }
 }
 
 struct ListCalculatorInputView_Previews: PreviewProvider {
     static var previews: some View {
         ListCalculatorInputView(item: .constant(Item()))
-            .previewLayout(.fixed(width: 220, height: 250))
+            .previewLayout(.fixed(width: 220, height: 220))
     }
 }
