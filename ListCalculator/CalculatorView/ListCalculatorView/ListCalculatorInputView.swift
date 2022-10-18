ead6debef71855cf1840f25a622501f4fa07ec58
seungjin.ll <devilzcough@gmail.com>
2022-10-18 23:52:42 +0900

    feat :⚡️ inputView 배경 없애기 및 blur 효과
    - iOS16 이상 지원. InputView 배경 없애기
    - InputView 뒷쪽에 Blur 효과 추가하기

--- ListCalculator/CalculatorView/ListCalculatorView/ListCalculatorInputView.swift
+++ ListCalculator/CalculatorView/ListCalculatorView/ListCalculatorInputView.swift
@@ -21,40 +21,45 @@ struct ListCalculatorInputView: View {
     }
     
     var body: some View {
-        List {
-            TextField("이름", text: $item.name)
-            TextField("금액", value: $item.price, formatter: NumberFormatter())
-            Stepper(value: $item.count, in: 1...100) {
-                Text("\(item.count)")
-            }
-            
-            HStack {
-                Text("금액 포함 여부")
-                CheckButton(isChecked: $item.isChecked)
-                    .frame(width: 20, height: 20)
-            }
-            .frame(maxWidth: .infinity)
-            
-            HStack {
-                Button("Cancel") {
-                    print("Cancel")
-                    item = copiedItem
-                    hasSelection = nil
+        if #available(iOS 16.0, *) {
+            List {
+                TextField("이름", text: $item.name)
+                TextField("금액", value: $item.price, formatter: NumberFormatter())
+                Stepper(value: $item.count, in: 1...100) {
+                    Text("\(item.count)")
+                }
+                
+                HStack {
+                    Text("금액 포함 여부")
+                    CheckButton(isChecked: $item.isChecked)
+                        .frame(width: 20, height: 20)
                 }
                 .frame(maxWidth: .infinity)
-                .buttonStyle(BorderlessButtonStyle())
                 
-                Divider()
-                
-                Button("Done") {
-                    print("Done")
-                    hasSelection = nil
+                HStack {
+                    Button("Cancel") {
+                        print("Cancel")
+                        item = copiedItem
+                        hasSelection = nil
+                    }
+                    .frame(maxWidth: .infinity)
+                    .buttonStyle(BorderlessButtonStyle())
+                    
+                    Divider()
+                    
+                    Button("Done") {
+                        print("Done")
+                        hasSelection = nil
+                    }
+                    .frame(maxWidth: .infinity)
+                    .buttonStyle(BorderlessButtonStyle())
                 }
-                .frame(maxWidth: .infinity)
-                .buttonStyle(BorderlessButtonStyle())
+                .listRowInsets(EdgeInsets())
+                .background(Color.yellow)
             }
-            .listRowInsets(EdgeInsets())
-            .background(Color.yellow)
+            .scrollContentBackground(.hidden)
+        } else {
+            // Fallback on earlier versions
         }
     }
 }
---
+++ ListCalculator/CalculatorView/VisualEffect.swift
@@ -0,0 +1,23 @@
+//
+//  BlurEffect.swift
+//  ListCalculator
+//
+//  Created by jiniz.ll on 2022/10/18.
+//
+
+import UIKit
+import SwiftUI
+
+struct VisualEffect: UIViewRepresentable {
+    
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+        
+        UIVisualEffectView()
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+        
+        uiView.effect = effect
+    }
+}
