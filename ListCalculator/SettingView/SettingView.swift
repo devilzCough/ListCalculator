ec4cf79c7a1ac21bf81a8b6624877015d753392d
seungjin.ll <devilzcough@gmail.com>
2022-11-16 14:35:22 +0900

    feat :⚡️ 설정화면 구현
    - 기본적으로 필요하다고 생각되는 설정 메뉴들을 넣어보았다.

--- ListCalculator/SettingView/SettingView.swift
+++ ListCalculator/SettingView/SettingView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct SettingView: View {
+    
+    struct Setting: Hashable, Identifiable {
+        let id = UUID()
+        let content: String
+    }
+    
+    struct SettingType: Identifiable {
+        let id = UUID()
+        let title: String
+        let settings: [Setting]
+    }
+    
+    private let settings = [
+        
+        SettingType(title: "일반", settings: [
+            Setting(content: "언어"),
+            Setting(content: "단위")
+        ]),
+        SettingType(title: "백업", settings: [
+            Setting(content: "동기화")
+        ]),
+        SettingType(title: "지원", settings: [
+            Setting(content: "리스트 계산기 사용법"),
+            Setting(content: "의견 보내기")
+        ]),
+        SettingType(title: "기타", settings: [
+            Setting(content: "About")
+        ])
+    ]
+    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(settings) { section in
+                Section(section.title) {
+                    ForEach(section.settings) { setting in
+                        Text(setting.content)
+                    }
+                }
+            }
+            .navigationTitle("설정")
+        }
     }
 }
 
